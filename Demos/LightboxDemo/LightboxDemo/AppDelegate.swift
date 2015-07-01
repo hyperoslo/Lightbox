042042f13a4a3cc6a4f28c23e24a0f7cd31d4343
Vadym Markov <markov.vadym@gmail.com>
2015-07-01 14:44:40 +0200

    Refactor map in demo

--- Demos/LightboxDemo/LightboxDemo/AppDelegate.swift
+++ Demos/LightboxDemo/LightboxDemo/AppDelegate.swift
@@ -11,8 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-    let imageNames = ["photo1", "photo2", "photo3"]
-    let images = imageNames.map { UIImage(named: $0)! }
+    let images = ["photo1", "photo2", "photo3"].map { UIImage(named: $0)! }
     let controller = LightboxController(images: images)
 
     window!.rootViewController = controller

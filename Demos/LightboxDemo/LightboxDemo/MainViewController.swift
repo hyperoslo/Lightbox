2d73ecbf2e5c7fae5b5fa514dc9ca3295abe0166
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-07-15 14:50:33 +0200

    Added the first piece of code

--- Demos/LightboxDemo/LightboxDemo/MainViewController.swift
+++ Demos/LightboxDemo/LightboxDemo/MainViewController.swift
@@ -2,5 +2,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-  
+  lazy var galleryButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Show the gallery", forState: .Normal)
+    button.tintColor = UIColor(red:0.98, green:0.18, blue:0.36, alpha:1)
+    button.titleLabel!.font = UIFont(name: "AvenirNextCondensed-DemiBold ", size: 24)
+    button.addTarget(self, action: "galleryButtonDidPress:", forControlEvents: .TouchUpInside)
+
+    return button
+    }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.addSubview(galleryButton)
+  }
+
+  // MARK: Action handlers
+
+  func galleryButtonDidPress(button: UIButton) {
+    
+  }
 }

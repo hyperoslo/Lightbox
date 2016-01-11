270f4e887fb74d1273ba65c56f2d06e917d7e3fc
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-11 10:35:30 +0100

    Adds the button

--- Demos/DemoLightbox/DemoLightbox/ViewController.swift
+++ Demos/DemoLightbox/DemoLightbox/ViewController.swift
@@ -1,25 +1,29 @@
-//
-//  ViewController.swift
-//  DemoLightbox
-//
-//  Created by Ramon Gilabert Llop on 1/11/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+  lazy var showButton: UIButton = { [unowned self] in
+    let button = UIButton()
+    button.addTarget(self, action: "showLightbox", forControlEvents: .TouchUpInside)
+    button.setTitle("Show me the lightbox", forState: .Normal)
+    button.setTitleColor(UIColor(red:0.47, green:0.6, blue:0.13, alpha:1), forState: .Normal)
+    button.titleLabel?.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 30)
+    button.frame = UIScreen.mainScreen().bounds
+
+    return button
+    }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    view.backgroundColor = UIColor.whiteColor()
+    view.addSubview(showButton)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  // MARK: - Action methods
+
+  func showLightbox() {
+    // TODO: - Lightbox
   }
-
-
 }
 

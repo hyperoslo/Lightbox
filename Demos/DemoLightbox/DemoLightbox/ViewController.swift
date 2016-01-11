a259b35826e523f81eea50861ea8d609fed2d3e0
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-11 09:50:07 +0100

    Adds the files for the example

---
+++ Demos/DemoLightbox/DemoLightbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoLightbox
+//
+//  Created by Ramon Gilabert Llop on 1/11/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

495fc0aab44668d45cc6e3971d75f9ff2f7aaeef
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-11 15:26:48 +0100

    Adds the different mandatory methods

--- Source/LightboxTransition.swift
+++ Source/LightboxTransition.swift
@@ -3,3 +3,25 @@ import UIKit
 class LightboxTransition: NSObject {
   
 }
+
+extension LightboxTransition: UIViewControllerAnimatedTransitioning {
+
+  func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+    
+  }
+
+  func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+
+  }
+}
+
+extension LightboxTransition: UIViewControllerTransitioningDelegate {
+
+}
+
+extension LightboxTransition: UIViewControllerInteractiveTransitioning {
+
+  func startInteractiveTransition(transitionContext: UIViewControllerContextTransitioning) {
+    
+  }
+}

3312be7a70e7c73b1a310d831ca3503967fcba73
Vadym Markov <markov.vadym@gmail.com>
2016-01-27 14:19:09 +0100

    Add double tap recognizer

--- Source/PageView.swift
+++ Source/PageView.swift
@@ -46,18 +46,74 @@ class PageView: UIScrollView {
     userInteractionEnabled = true
     showsHorizontalScrollIndicator = false
     showsVerticalScrollIndicator = false
-    addGestureRecognizer(panGestureRecognizer)
+
+    let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+    doubleTapRecognizer.numberOfTapsRequired = 2
+    doubleTapRecognizer.numberOfTouchesRequired = 1
+    addGestureRecognizer(doubleTapRecognizer)
+  }
+
+  // MARK: - Recognizers
+
+  func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+    let pointInView = recognizer.locationInView(imageView)
+    let newZoomScale = zoomScale > minimumZoomScale
+      ? minimumZoomScale
+      : maximumZoomScale
+
+    let width = bounds.size.width / newZoomScale
+    let height = bounds.size.height / newZoomScale
+    let x = pointInView.x - (width / 2.0)
+    let y = pointInView.y - (height / 2.0)
+
+    let rectToZoomTo = CGRectMake(x, y, width, height)
+
+    zoomToRect(rectToZoomTo, animated: true)
   }
 
   // MARK: - Layout
 
   func configureLayout(frame: CGRect) {
-    imageView.frame = frame
-    imageView.frame.size.width = frame.width
-    imageView.frame.origin.x = 0
-
     self.frame = frame
-    contentSize = imageView.frame.size
+    contentSize = frame.size
+    imageView.frame = frame
+
+    let imgViewSize = imageView.frame.size
+    let imageSize = imageView.image!.size
+
+    zoomScale = LightboxConfig.config.zoom.minimumScale
+
+    var realImgSize: CGSize
+    if(imageSize.width / imageSize.height > imgViewSize.width / imgViewSize.height) {
+      realImgSize = CGSizeMake(imgViewSize.width, imgViewSize.width / imageSize.width * imageSize.height);
+    }
+    else {
+      realImgSize = CGSizeMake(imgViewSize.height / imageSize.height * imageSize.width, imgViewSize.height);
+    }
+
+    var fr = CGRectMake(0, 0, 0, 0)
+    fr.size = realImgSize
+    imageView.frame = fr
+    centerImageView()
+  }
+
+  func centerImageView() {
+    let boundsSize = bounds.size
+    var contentsFrame = imageView.frame
+
+    if contentsFrame.size.width < boundsSize.width {
+      contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+    } else {
+      contentsFrame.origin.x = 0.0
+    }
+
+    if contentsFrame.size.height < boundsSize.height {
+      contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+    } else {
+      contentsFrame.origin.y = 0.0
+    }
+
+    imageView.frame = contentsFrame
   }
 }
 
@@ -67,5 +123,9 @@ extension PageView: UIScrollViewDelegate {
 
   func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
     return imageView
+  }
+
+  func scrollViewDidZoom(scrollView: UIScrollView) {
+    centerImageView()
   }
 }

a6119a3cc0b2b582849f7138637f837e817ee71e
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-09-28 16:33:28 +0200

    Updates the demo

--- Demos/DemoLightbox/DemoLightbox/ViewController.swift
+++ Demos/DemoLightbox/DemoLightbox/ViewController.swift
@@ -5,12 +5,12 @@ class ViewController: UIViewController {
 
   lazy var showButton: UIButton = { [unowned self] in
     let button = UIButton()
-    button.addTarget(self, action: #selector(showLightbox), forControlEvents: .TouchUpInside)
-    button.setTitle("Show me the lightbox", forState: .Normal)
-    button.setTitleColor(UIColor(red:0.47, green:0.6, blue:0.13, alpha:1), forState: .Normal)
+    button.addTarget(self, action: #selector(showLightbox), for: .touchUpInside)
+    button.setTitle("Show me the lightbox", for: UIControlState())
+    button.setTitleColor(UIColor(red:0.47, green:0.6, blue:0.13, alpha:1), for: UIControlState())
     button.titleLabel?.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 30)
-    button.frame = UIScreen.mainScreen().bounds
-    button.autoresizingMask = [.FlexibleTopMargin, .FlexibleLeftMargin, .FlexibleRightMargin, .FlexibleBottomMargin]
+    button.frame = UIScreen.main.bounds
+    button.autoresizingMask = [.flexibleTopMargin, .flexibleLeftMargin, .flexibleRightMargin, .flexibleBottomMargin]
 
     return button
     }()
@@ -18,8 +18,8 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    view.autoresizingMask = [.FlexibleTopMargin, .FlexibleLeftMargin, .FlexibleRightMargin, .FlexibleBottomMargin]
-    view.backgroundColor = UIColor.whiteColor()
+    view.autoresizingMask = [.flexibleTopMargin, .flexibleLeftMargin, .flexibleRightMargin, .flexibleBottomMargin]
+    view.backgroundColor = UIColor.white
     view.addSubview(showButton)
   }
 
@@ -34,19 +34,19 @@ class ViewController: UIViewController {
       LightboxImage(
         image: UIImage(named: "photo2")!,
         text: "",
-        videoURL: NSURL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        videoURL: URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
       ),
       LightboxImage(
         image: UIImage(named: "photo3")!,
         text: "Some very long lorem ipsum text."
       ),
-      LightboxImage(imageURL: NSURL(string: "https://cdn.arstechnica.net/2011/10/05/iphone4s_sample_apple-4e8c706-intro.jpg")!)
+      LightboxImage(imageURL: URL(string: "https://cdn.arstechnica.net/2011/10/05/iphone4s_sample_apple-4e8c706-intro.jpg")!)
     ]
 
     let controller = LightboxController(images: images)
     controller.dynamicBackground = true
 
-    presentViewController(controller, animated: true, completion: nil)
+    present(controller, animated: true, completion: nil)
   }
 }
 

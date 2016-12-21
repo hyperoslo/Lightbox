b43b61689b0ae2f7fd6c9070d26caf72c92783f6
Aashish Dhawan <aashishdhawan@gmail.com>
2016-12-21 16:23:41 +0530

    Removing dependency on Hue

---
+++ Source/Library/UIColor+Hex.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+public extension UIColor {
+    
+    convenience init(hex string: String) {
+        var hex = string.hasPrefix("#")
+            ? String(string.characters.dropFirst())
+            : string
+        guard hex.characters.count == 3 || hex.characters.count == 6
+            else {
+                self.init(white: 1.0, alpha: 0.0)
+                return
+        }
+        if hex.characters.count == 3 {
+            for (index, char) in hex.characters.enumerated() {
+                hex.insert(char, at: hex.index(hex.startIndex, offsetBy: index * 2))
+            }
+        }
+        
+        self.init(
+            red:   CGFloat((Int(hex, radix: 16)! >> 16) & 0xFF) / 255.0,
+            green: CGFloat((Int(hex, radix: 16)! >> 8) & 0xFF) / 255.0,
+            blue:  CGFloat((Int(hex, radix: 16)!) & 0xFF) / 255.0, alpha: 1.0)
+    }
+    
+    public func alpha(_ value: CGFloat) -> UIColor {
+        return withAlphaComponent(value)
+    }
+}

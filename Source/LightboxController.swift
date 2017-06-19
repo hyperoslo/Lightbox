03be7479f4e591ea1836fb3188fff4a4e5029094
Vitaliy Tim <post.vitaliy@gmail.com>
2017-06-19 11:50:09 +0300

    Fixes an issue with Navigation Bar, which had wrong position after the lightbox has been dismissed

--- Source/LightboxController.swift
+++ Source/LightboxController.swift
@@ -199,8 +199,8 @@ open class LightboxController: UIViewController {
     }
   }
 
-  open override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
+  open override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
 
     if LightboxConfig.hideStatusBar {
       UIApplication.shared.setStatusBarHidden(statusBarHidden, with: .fade)

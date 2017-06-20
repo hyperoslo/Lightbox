62d24d3df18597d14d76d373b2987ec1e36b3462
Vitaliy Tim <post.vitaliy@gmail.com>
2017-06-20 10:00:57 +0300

    self captured weakly now, so it doesn't retains together with the received image in case the preview has been dismissed before image is loaded

--- Source/LightboxImage.swift
+++ Source/LightboxImage.swift
@@ -26,8 +26,8 @@ open class LightboxImage {
       imageView.image = image
       completion?(image)
     } else if let imageURL = imageURL {
-      LightboxConfig.loadImage(imageView, imageURL) { error, image in
-        self.image = image
+      LightboxConfig.loadImage(imageView, imageURL) { [weak self] error, image in
+        self?.image = image
         completion?(image)
       }
     }

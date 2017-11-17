2cb8b9fd98d2800b6f0cc8c0bfaeec16f4167f36
Khoa Pham <onmyway133@gmail.com>
2017-11-17 15:04:31 +0100

    Expose LightboxConfig.loadImage. Use Imaginary by default

--- Source/LightboxConfig.swift
+++ Source/LightboxConfig.swift
@@ -2,6 +2,7 @@ import UIKit
 import Hue
 import AVKit
 import AVFoundation
+import Imaginary
 
 public class LightboxConfig {
   /// Whether to show status bar while Lightbox is presented
@@ -45,6 +46,21 @@ public class LightboxConfig {
     
     task.resume()
     
+  }
+
+  /// How to load image onto UIImageView
+  public static var loadImage: (UIImageView, URL, ((UIImage?) -> Void)?) -> Void =
+    { (imageView, imageURL , completion) in
+
+    // Use Imaginary by default
+    imageView.setImage(url: imageURL, placeholder: nil, completion: { result in
+      switch result {
+      case .value(let image):
+        completion?(image)
+      case .error:
+        completion?(nil)
+      }
+    })
   }
 
   /// Indicator is used to show while image is being fetched

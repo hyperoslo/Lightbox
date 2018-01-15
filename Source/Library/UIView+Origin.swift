6971619ce696260ff0b3806b17ca00699a9bc2b7
Luís Martins <lmsmartins@gmail.com>
2018-01-15 15:32:45 -0100

    Add custom top paddings for the header view due to differences in portrait and landscape mode for iPhone X.

--- Source/Library/UIView+Origin.swift
+++ Source/Library/UIView+Origin.swift
@@ -16,6 +16,8 @@ extension UIView {
     
     var point = CGPoint(x: 0, y: 0)
     
+    let orientation = UIApplication.shared.statusBarOrientation
+    
     if let edges = getSafeAreaInsets() {
       var y = edges.top - statusBarHeight
       
@@ -27,17 +29,21 @@ extension UIView {
          * is 0 and the bar draws correctly. On the X, it's partially
          * covered by the notch, so we add a little more padding to it.
          */
-        
         y -= 8
       }
       
       point.y = y
       
       // Add a little of padding in the landscape orientation, due to the notch in iPhone X.
-      let orientation = UIApplication.shared.statusBarOrientation
       if orientation == .landscapeLeft || orientation == .landscapeRight {
         point.x = 24
       }
+    }
+    
+    if orientation == .portrait || orientation == .portraitUpsideDown {
+      point.y += 32
+    } else {
+      point.y += 16
     }
     
     return point

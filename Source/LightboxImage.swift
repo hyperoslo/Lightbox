20ea9e3e1cccc25bbea503b7c84c46248df5ac60
Igor Fedotov <igor.fedotov@brickmakers.de>
2018-02-13 18:56:07 +0100

    Added private init method on LightboxImage to support the stub

--- Source/LightboxImage.swift
+++ Source/LightboxImage.swift
@@ -10,6 +10,10 @@ open class LightboxImage {
   open var text: String
 
   // MARK: - Initialization
+  
+  internal init(text: String = "") {
+    self.text = text
+  }
 
   public init(image: UIImage, text: String = "", videoURL: URL? = nil) {
     self.image = image

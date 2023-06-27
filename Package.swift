9bcaf34d3786c38e6128f5c14364dad7329b4741
Davide Vincenzi <davide@vincenzi.de>
2023-06-27 10:28:08 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Lightbox",
+    platforms: [
+        .iOS(.v16),
+    ],
     products: [
         .library(
             name: "Lightbox",

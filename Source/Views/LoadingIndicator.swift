bc9eb3a8debb70e2e3ae4a773198c8d3b050c96e
Davide Vincenzi <davide@vincenzi.de>
2023-06-27 09:54:40 +0200

    Update LoadingIndicator.swift

--- Source/Views/LoadingIndicator.swift
+++ Source/Views/LoadingIndicator.swift
@@ -7,7 +7,7 @@ class LoadingIndicator: UIView {
   init() {
     super.init(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
 
-    backgroundColor = UIColor.darkGray
+    // backgroundColor = UIColor.darkGray
     layer.cornerRadius = bounds.size.width / 2
     clipsToBounds = true
     alpha = 0

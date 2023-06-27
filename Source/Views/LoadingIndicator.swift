e323f554cd86fb7e2d23d29cc62703f35ad59260
Davide Vincenzi <davide@vincenzi.de>
2023-06-27 10:21:28 +0200

    Update LoadingIndicator.swift

--- Source/Views/LoadingIndicator.swift
+++ Source/Views/LoadingIndicator.swift
@@ -13,7 +13,7 @@ class LoadingIndicator: UIView {
     alpha = 0
 
     indicator = UIActivityIndicatorView()
-    indicator.style = .whiteLarge
+    indicator.style = .large
     indicator.startAnimating()
 
     addSubview(indicator)
